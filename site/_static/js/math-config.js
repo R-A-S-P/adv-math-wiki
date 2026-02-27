08d02fd76169475bae3600d5ecc9209ffc07554c
R-A-S-P <1981641498@qq.com>
2026-02-27 23:47:02 +0800

    一堆修改

--- site/_static/js/math-config.js
+++ site/_static/js/math-config.js
@@ -19,10 +19,31 @@ window.MathJax = {
   loader: {
     //load: ['[tex]/boldsymbol'],                       // 预加载的扩展包
   },
+  startup: {
+    ready: () => {
+      // 动态获取当前页面的基础路径
+      const baseUrl = window.location.pathname.split('/').slice(0, 1).join('/'); 
+      // 或者更简单：如果你的 site_url 是 https://r-a-s-p.github.io/adv-math-wiki/
+      // 那么基础路径就是 '/adv-math-wiki'
+      //const baseUrl = '/adv-math-wiki'; 
+      
+      // 设置 fontURL 和 dynamicPrefix
+      MathJax.config.chtml = {
+        font: 'mathjax-newcm', // 或你的字体
+        fontURL: baseUrl + '/assets/vendor/MathJax-4.1.1/mathjax-fonts/mathjax-newcm-font/woff-v2',
+        dynamicPrefix: baseUrl + '/assets/vendor/MathJax-4.1.1/mathjax-fonts/mathjax-newcm-font/dynamic',
+        mtextInheritFont: true, // 设置\text命令内的字体 或使用 mtextFontInherit: true (取决于MathJax版本)
+        matchFontHeight: false
+      };
+      
+      // 调用默认的 ready 函数
+      MathJax.startup.defaultReady();
+    }
+  },
   chtml: {
-    font: 'mathjax-newcm',
-    fontURL: '/adv-math-wiki/assets/mathjax-fonts/mathjax-newcm-font/woff-v2',  // 字体文件地址（可换国内源）
-    dynamicPrefix: '/adv-math-wiki/assets/mathjax-fonts/mathjax-newcm-font/dynamic',
+    //font: 'mathjax-newcm',
+    //fontURL: '/mathjax-fonts/mathjax-newcm-font/woff-v2',  // 字体文件地址（可换国内源）
+    //dynamicPrefix: '{{ base_url }}/mathjax-fonts/mathjax-newcm-font/dynamic',
     mtextInheritFont: true, // 设置\text命令内的字体 或使用 mtextFontInherit: true (取决于MathJax版本)
     matchFontHeight: false
   }
